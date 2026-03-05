2c6e3b955a47ac234183855ebbd0c9f406ef05ac
Kassius Capito <kassiuscapito@icloud.com>
2026-03-05 15:59:16 -0800

    new emojis...

--- components/AcronymBox.jsx
+++ components/AcronymBox.jsx
@@ -1,10 +1,14 @@
 function AcronymBox(props) {
     function generateAcronym(text) {
-        return text.split(/\W+/).map(word => word[0]).join('').toUpperCase()
+        return text.split(/(?<=[\s\-_/])/).map(word => word[0]).join('').toUpperCase()
+    }
+    function broThrewInAFunEmoji() {
+        const emojis = ["9996", "129344", "129515", 0x1F979, 0x1FAAB, 0x1F38B]
+        return emojis[Math.floor(Math.random()*emojis.length)]
     }
     return (
         <div id="AcronymBox">
-            <h2>{generateAcronym(props.text)}</h2>
+            <h2>{generateAcronym(props.text) + " " + String.fromCodePoint(broThrewInAFunEmoji())}</h2>
         </div>
     )
 }
